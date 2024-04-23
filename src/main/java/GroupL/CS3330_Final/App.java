156ae6b81005ae1fafc24567eb7131c6529c3ce2
Kaelyn Crews <crewskaelyn@gmail.com>
2024-04-23 15:39:52 -0500

    Creating maven project

---
+++ src/main/java/GroupL/CS3330_Final/App.java
@@ -0,0 +1,13 @@
+package GroupL.CS3330_Final;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
