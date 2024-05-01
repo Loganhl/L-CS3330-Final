697d027310f205da8741165a39d818e4bd84001b
Kaelyn Crews <crewskaelyn@gmail.com>
2024-05-01 16:14:25 -0500

    creating the FoodItemInterface

--- src/main/java/FoodItem/FoodItemInterface.java
+++ src/main/java/FoodItem/FoodItemInterface.java
@@ -1,5 +1,12 @@
 package FoodItem;
 
 public interface FoodItemInterface {
-
+	String getFoodName();
+	String getServingSize();
+	int getCalories();
+	double getFats();
+	int getCholesterol();
+	int getSodium();
+	int getCarbs();
+	int getProtein();
 }
