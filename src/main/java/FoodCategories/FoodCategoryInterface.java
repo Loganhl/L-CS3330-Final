f0995be64d32d416aff696d4eb0e673c051021a5
kylewilliams <kylewilliams@localhost>
2024-05-02 18:16:09 -0500

    Added methods to FoodCategoryInterface - KTWMF6

--- src/main/java/FoodCategories/FoodCategoryInterface.java
+++ src/main/java/FoodCategories/FoodCategoryInterface.java
@@ -1,5 +1,13 @@
 package FoodCategories;
 
 public interface FoodCategoryInterface {
+	//Get the name of the food category
+	String getName();
+	//Get the Key Message of Food Category
+	String getKeyMessage();
+	//Get IncludedFoods 
+	String getIncludedFoods();
+	//Get Recommended amount of consumption
+	String getRecommendedAmount();
 
 }
