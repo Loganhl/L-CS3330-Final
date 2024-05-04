56f7ea2ad3e49351f26c87fb8f54bf7fe0d355ba
kylewilliams <kylewilliams@localhost>
2024-05-04 13:23:24 -0500

    added class provides info on fruits category - KTWMF6

--- src/main/java/FoodCategories/FruitsCategory.java
+++ src/main/java/FoodCategories/FruitsCategory.java
@@ -1,5 +1,14 @@
 package FoodCategories;
 
 public class FruitsCategory {
+	//method that provides information about fruits category
+	public String getFruitsCategoryInfo() {
+		return "Fruits may be fresh, canned, frozen, or dried, and may be whole, cut-up, or pureed.";
+	}
+	
+	//method that provides nutrition recommendations for fruits category 
+	public String getFruitsNutritionalReccomendations() {
+		return "The amount of fruit you need to eat depends on age, sex, height, weight, and level of physical activity.";
+	}
 
 }
