08a1a651feffc742cfbf2ae9a96dd123b524ef3a
Loganhl <lhunterlimbaugh@gmail.com>
2024-05-06 17:30:49 -0500

    Fixed an error.

--- src/main/java/FoodItem/Dairy.java
+++ src/main/java/FoodItem/Dairy.java
@@ -4,3 +4,4 @@ public class Dairy extends Food {
 public Dairy(String foodName, String servingSize, double calories, double fats, double cholesterol, double sodium, double carbohydrates, double protein, String recipe, String nutritionalRecommendation) {
 	super(foodName, foodName, nutritionalRecommendation, calories, fats, calories, calories, calories, fats, foodName, foodName);
 }
+}
