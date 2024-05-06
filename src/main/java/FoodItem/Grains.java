494f41eebf63f956ec5f917d38f4efd38285077d
Loganhl <lhunterlimbaugh@gmail.com>
2024-05-06 17:29:16 -0500

    Fixed an error.

--- src/main/java/FoodItem/Grains.java
+++ src/main/java/FoodItem/Grains.java
@@ -5,3 +5,4 @@ public class Grains extends Food {
 public Grains(String foodName, String servingSize, double calories, double fats, double cholesterol, double sodium, double carbohydrates, double protein, String recipe, String nutritionalRecommendation) {
 	super(foodName, foodName, nutritionalRecommendation, calories, fats, calories, calories, calories, fats, foodName, foodName);
 }
+}
--- src/main/java/GroupL/CS3330_Final/menu.java
+++ src/main/java/GroupL/CS3330_Final/menu.java
@@ -7,7 +7,7 @@ import FoodItem.Fruit;
 
 public class menu {
     
-    public static void start(String[] args) {
+    public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
         boolean displayMenu = true;
