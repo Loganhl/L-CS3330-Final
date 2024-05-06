01922cdc25c6acc763b38c19884345dd3aa010fc
Logan Limbaugh <91567972+Loganhl@users.noreply.github.com>
2024-05-06 17:25:28 -0500

    Update Grains.java

--- src/main/java/FoodItem/Grains.java
+++ src/main/java/FoodItem/Grains.java
@@ -1,5 +1,7 @@
 package FoodItem;
 
-public class Grains {
 
+public class Grains extends Food {
+public Grains(String foodName, String servingSize, double calories, double fats, double cholesterol, double sodium, double carbohydrates, double protein, String recipe, String nutritionalRecommendation) {
+	super(foodName, foodName, nutritionalRecommendation, calories, fats, calories, calories, calories, fats, foodName, foodName);
 }
