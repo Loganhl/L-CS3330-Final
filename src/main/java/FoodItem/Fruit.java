d0fa8df2879d7da9f936c2905815b0ee9868e403
Logan Limbaugh <91567972+Loganhl@users.noreply.github.com>
2024-05-06 17:06:14 -0500

    Update Fruit.java
    Fixed constructor with correct values. Added printRecipe method as a placeholder recipe method.

--- src/main/java/FoodItem/Fruit.java
+++ src/main/java/FoodItem/Fruit.java
@@ -2,15 +2,23 @@ package FoodItem;
 
 
 public class Fruit extends Food {
-public Fruit() {
-	super();
+public Fruit(String foodName, String servingSize, double calories, double fats, double cholesterol, double sodium, double carbohydrates, double protein, String recipe, String nutritionalRecommendation) {
+	super(foodName, foodName, nutritionalRecommendation, calories, fats, calories, calories, calories, fats, foodName, foodName);
 }
 
-public Fruit(Fruit food) {
-    super(food);
-}
+
 
 // Method that prints all of the Fruits
+
+public static void printFruitRecipes() {
+    
+    System.out.println("===============================");
+    System.out.println("Common Fruit Recipes:");
+    System.out.println("1. Fruit Salad");
+    System.out.println("2. Smoothie");
+    System.out.println("3. Fruit Parfait");
+    System.out.println("===============================");
+}
 
 // Method that prints nutritional Values based off of .json values.
 
