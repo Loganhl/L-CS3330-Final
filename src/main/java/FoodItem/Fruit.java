d37db2920da93ed5153b887700fd31fe029170f6
Loganhl <lhunterlimbaugh@gmail.com>
2024-05-06 16:46:39 -0500

    Setting up Food Category classes.

--- src/main/java/FoodItem/Fruit.java
+++ src/main/java/FoodItem/Fruit.java
@@ -1,5 +1,19 @@
 package FoodItem;
 
-public class Fruit {
+
+public class Fruit extends Food {
+public Fruit() {
+	super();
+}
+
+public Fruit(Fruit food) {
+    super(food);
+}
+
+// Method that prints all of the Fruits
+
+// Method that prints nutritional Values based off of .json values.
+
+//Method that prints common recipe's with fruits based off of .json values.
 
 }
--- src/main/java/FoodItem/Protein.java
+++ src/main/java/FoodItem/Protein.java
@@ -1,5 +1,13 @@
 package FoodItem;
 
-public class Protein {
+
+public class Protein extends Food {
+public Protein() {
+	super();
+}
+
+public Protein(Protein food) {
+    super(food);
+}
 
 }
