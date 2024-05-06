55a4c4e0f878dc6df17ac754ef9a0f2114804090
Logan Limbaugh <91567972+Loganhl@users.noreply.github.com>
2024-05-06 17:26:05 -0500

    Update Vegetables.java

--- src/main/java/FoodItem/Vegetables.java
+++ src/main/java/FoodItem/Vegetables.java
@@ -1,5 +1,7 @@
 package FoodItem;
 
-public class Vegetables {
 
+public class Vegetables extends Food {
+public Vegetables(String foodName, String servingSize, double calories, double fats, double cholesterol, double sodium, double carbohydrates, double protein, String recipe, String nutritionalRecommendation) {
+	super(foodName, foodName, nutritionalRecommendation, calories, fats, calories, calories, calories, fats, foodName, foodName);
 }
