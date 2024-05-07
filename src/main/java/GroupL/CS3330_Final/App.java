a0a09e96a5b97ab89b2e8ff93c4a9ac9682c7916
crewskaelyn <116387429+crewskaelyn@users.noreply.github.com>
2024-05-06 19:10:41 -0500

    Update App.java
    changing app.java to parse the csv file and start up the menu

--- src/main/java/GroupL/CS3330_Final/App.java
+++ src/main/java/GroupL/CS3330_Final/App.java
@@ -1,7 +1,17 @@
 package GroupL.CS3330_Final;
 
+import FoodItem.FoodManager;
+
 public class App {
     public static void main(String[] args) {
-        menu.start(args);
+        try {
+            FoodManager foodManager = new FoodManager();
+            boolean fileReadStatus = foodManager.initializeFood();
+            if (fileReadStatus) {
+                menu.start(args);
+            }
+        } catch (Exception e) {
+            System.err.println("Error running the program: "+ e.getMessage());
+        }
     }
 }
