b3a0b25d5faa5551cdfba906c0c8bc0edf8b1370
KTWMF6 <112215223+KTWMF6@users.noreply.github.com>
2024-05-06 20:16:17 -0500

    Update Fruit.java
    added smoothie rec

--- src/main/java/FoodItem/Fruit.java
+++ src/main/java/FoodItem/Fruit.java
@@ -16,7 +16,7 @@ public static void printFruitRecipes() {
     
 
     System.out.println("1. Fruit Salad: Ingredients: \n-(Assorted Fruits e.g., strawberries, blueberries, grapes, kiwi, pineapple) : \n- Instructions: \n1. Wash and chop fruits. \n2. Combine Fruits in bowl. \n3. Toss gently to mix. \n4. Serve chilled.";
-    System.out.println("2. Smoothie");
+    System.out.println("2. Smoothie: Ingredients: \n- (Assorted Fruits e.g., strawberries, blueberries, mango), Greek yogurt, ice cubes : \n- Instructions: \n1. Wash and prepare the fruit. \n2. Place fruit in blender. \n3. Add Greek yogurt in blender. \n4. Add ice cubes to blender. \n5. Blend until smooth and creamy";
     System.out.println("3. Fruit Parfait");
 
 }
